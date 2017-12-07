03e8d9040931c6421b9db0ffd12904e2920f8107
SPEEDO\target <daniel.b.coughlan@mycit.ie>
2017-12-07 14:07:34 +0000

    Initial commit

---
+++ FlowersApp/src/main/java/com/flowers/demo/FlowersAppApplication.java
@@ -0,0 +1,12 @@
+package com.flowers.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlowersAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FlowersAppApplication.class, args);
+	}
+}
