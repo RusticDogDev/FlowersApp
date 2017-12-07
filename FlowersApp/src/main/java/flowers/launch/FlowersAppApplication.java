a393139da2f18896848beddeca25a4a34612bef8
SPEEDO\target <daniel.b.coughlan@mycit.ie>
2017-12-07 18:08:03 +0000

    login

---
+++ FlowersApp/src/main/java/flowers/launch/FlowersAppApplication.java
@@ -0,0 +1,18 @@
+package flowers.launch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.stereotype.Controller;
+
+@EnableAutoConfiguration
+@SpringBootApplication
+@ComponentScan
+@Controller
+public class FlowersAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FlowersAppApplication.class, args);
+	}
+}
