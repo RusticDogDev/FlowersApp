3adc5886da44fe6cf7ae826cc3a291998ad7e7d4
SPEEDO\target <daniel.b.coughlan@mycit.ie>
2017-12-10 16:49:07 +0000

    fix error

--- FlowersApp/src/main/java/flowers/launch/FlowersAppApplication.java
+++ FlowersApp/src/main/java/flowers/launch/FlowersAppApplication.java
@@ -1,23 +1,13 @@
 package flowers.launch;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.CommandLineRunner;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
-import org.springframework.boot.autoconfigure.data.mongo.MongoDataAutoConfiguration;
-import org.springframework.boot.autoconfigure.mongo.MongoAutoConfiguration;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
-import org.springframework.stereotype.Controller;
-import flowers.launch.models.User;
 import flowers.launch.repositories.UserRepository;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.CommandLineRunner;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication//(exclude = {MongoAutoConfiguration.class, MongoDataAutoConfiguration.class})
+@SpringBootApplication
 public class FlowersAppApplication extends WebMvcConfigurerAdapter {
 		
 	@Autowired
@@ -25,23 +15,5 @@ public class FlowersAppApplication extends WebMvcConfigurerAdapter {
 
 	public static void main(String[] args) {
 		SpringApplication.run(FlowersAppApplication.class, args);
-	}
-
-	@Override
-	public void run(String... arg0) throws Exception {
-		// TODO Auto-generated method stub
-		
-		userRepository.deleteAll();
-		userRepository.save(new User((long) 1123432,"regular", "kieran2","guess"));
-
-		
-		System.out.println("Userss found with findAll():");
-		System.out.println("-------------------------------");
-		for (User user : userRepository.findAll()) {
-			System.out.println(user);
-		}
-		System.out.println();
-		
-	}
-	
+	}	
 }
