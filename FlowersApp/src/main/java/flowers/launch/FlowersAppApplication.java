c49305491c118cff5586912d1756b0faf09062ad
KieranDonovan <34342731+KieranDonovan@users.noreply.github.com>
2017-12-10 15:52:45 +0000

    application

--- FlowersApp/src/main/java/flowers/launch/FlowersAppApplication.java
+++ FlowersApp/src/main/java/flowers/launch/FlowersAppApplication.java
@@ -5,16 +5,39 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
 import org.springframework.stereotype.Controller;
+import flowers.launch.models.User;
+import flowers.launch.repositories.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@EnableAutoConfiguration
 @SpringBootApplication
-@ComponentScan
-@Controller
-public class FlowersAppApplication {
+public class FlowersAppApplication implements CommandLineRunner {
+	
+	@Autowired
+	private UserRepository userRepository;
 
 	public static void main(String[] args) {
 		SpringApplication.run(FlowersAppApplication.class, args);
+	}
+
+	@Override
+	public void run(String... arg0) throws Exception {
+		// TODO Auto-generated method stub
+		
+		userRepository.deleteAll();
+		userRepository.save(new User((long) 1123432,"regular", "kieran2","guess"));
+
+		
+		System.out.println("Userss found with findAll():");
+		System.out.println("-------------------------------");
+		for (User user : userRepository.findAll()) {
+			System.out.println(user);
+		}
+		System.out.println();
+		
 	}
 	
 }
